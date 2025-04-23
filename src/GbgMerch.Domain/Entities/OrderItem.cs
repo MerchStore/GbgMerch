d574761fbbe82e37ab46b7fcaf1bde68abdfa65e
99amiqur <99amiqur@gafe.molndal.se>
2025-04-23 14:29:45 +0200

    MER-63 New Product Knapp

---
+++ src/GbgMerch.Domain/Entities/OrderItem.cs
@@ -0,0 +1,12 @@
+namespace GbgMerch.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();         // Unikt ID för varje OrderItem
+    public string ProductName { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    // Räknas automatiskt: Pris * Antal
+    public decimal Subtotal => Price * Quantity;
+}
